0f9452946a0b278e451f450ed68699c5863f4a48
nxycdl <nxycdl@163.com>
2017-02-20 13:38:59 +0800

    add_resource

---
+++ src/components/line/lines.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="title">
+    <div class="line">
+    </div>
+    <div class="text"></div>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      text: {
+        type: String
+      }
+    },
+    data() {
+      return {
+
+      };
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "lines.style";
+</style>
