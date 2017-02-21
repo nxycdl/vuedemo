c96dcdcc08e7cb8c65e50bb932a73a82e518bc88
nxycdl <nxycdl@163.com>
2017-02-21 21:38:32 +0800

    'svg'

---
+++ src/components/lines/lines.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="title">
+    <div class="line">
+    </div>
+    <div class="text" @click="sendMsg">{{text}}{{seller.description}}</div>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      text: {
+        type: String
+      },
+      seller: {
+        type: Object
+      }
+    },
+    methods: {
+      sendMsg() {
+        console.log('xxxx');
+        this.$emit('id-selected', 1);
+      }
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import 'lines.styl';
+</style>
