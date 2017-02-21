db9291a17fb75cc35d58a74b93b05d6f839dc07a
nxycdl <nxycdl@163.com>
2017-02-21 23:14:55 +0800

    m“

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="shopcart">
+    <div class="content">
     <div class="content-left">
       <div class="logo-wrapper">
         <div class="logo">
-          <span class="icon-shopping_cart"></span>
+          <i class="icon-shopping_cart"></i>
         </div>
       </div>
       <div class="price"></div>
       <div class="desc"></div>
-      
+
     </div>
     <div class="content-right"></div>
+    </div>
   </div>
 </template>
 
